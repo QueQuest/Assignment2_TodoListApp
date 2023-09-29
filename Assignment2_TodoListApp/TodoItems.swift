876527ddbd61940a64e5402e5253ad86a741198d
Ethan Kim <jikim1803@gmail.com>
2023-09-28 20:33:45 -0400

    completed tasks change when mark done is hit

--- Assignment2_TodoListApp/TodoItems.swift
+++ Assignment2_TodoListApp/TodoItems.swift
@@ -6,6 +6,8 @@
 //
 
 import Foundation
+import SwiftUI
+import Combine
 
 class TodoItems: ObservableObject {
     
@@ -13,17 +15,41 @@ class TodoItems: ObservableObject {
      TODO (variables [min. 1] + initializer)
      Hint: The purpose of this class is to store a collection of TodoItem objects representing the added items. Do you know why? (has to do with each todo item's state)
      */
-    @Published var todoList: [TodoItem]
+    @Published var todoList: [TodoItem] {
+       willSet {
+            
+        } didSet {
+            self.bag = todoList.map { item in
+                item.objectWillChange.sink(receiveValue: {
+                    self.objectWillChange.send()
+                })
+            }
+        }
+    }
+    var bag: [AnyCancellable?] = []
     
     init () {
         todoList = []
+        
+        self.bag = todoList.map { item in
+            item.objectWillChange.sink(receiveValue: {
+                self.objectWillChange.send()
+            })
+        }
+        
+        /*
+        addItem(description: "blah blah", postAddAction: { description, row in
+            print("Added item number \(row) with description: \(description).")
+        })*/
     }
     /*
      TODO (functions [min. 1])
      */
     func addItem(description: String) {
         todoList.append(TodoItem(description: description))
+        
     }
+    
     
     
     
--- Assignment2_TodoListApp/TodoListScreen.swift
+++ Assignment2_TodoListApp/TodoListScreen.swift
@@ -16,39 +16,68 @@ struct TodoListScreen: View {
     @State var description: String = "";
     
     var items = ["1", "2", "3", "4"]
-        var body: some View {
-            VStack(spacing: 30) {
-                TextField(
-                    "Todo Description",
-                    text: $description
-                )
-                // this part should be inside todoItemScreen
-                VStack(spacing: 5){
-                    ForEach(todoList.todoList, id: \.description) { item in
-                        HStack{
-                            Text(item.description).foregroundColor( item.completed ? Color.green : Color.black)
-                            Button(action: {
-                                item.toggleCompleted();
-                            },label:{
-                                Text("Mark done");
-                            })
-                        }
-                        
+    var body: some View {
+        VStack(spacing: 30) {
+            TextFieldCopy(description: description)
+            
+            TextField(
+                "Todo Description",
+                text: $description
+            )
+            // this part should be inside todoItemScreen
+            VStack(spacing: 5){
+                ForEach(todoList.todoList, id: \.description) { item in
+                    HStack{
+                        Text(item.description).foregroundColor( item.completed ? Color.green : Color.black)
+                        Button(action: {
+                            item.toggleCompleted();
+                        },label:{
+                            Text("Mark done");
+                        })
+                        Button(action: {
+                            print("go to edit screen")
+                        },label:{
+                            Text("Edit");
+                        })
                     }
+                    
                 }
-                // end of part that should be inside todoItemScreen
-                Button(action: {
-                    if description.count > 0 {
-                        todoList.addItem(description: description);
-                    }
-                    description = "";
-                },label:{
-                    Text("Add")
-                })
             }
+            // end of part that should be inside todoItemScreen
+            Button(action: {
+                if description.count > 0 {
+                    todoList.addItem(description: description);
+                }
+                description = "";
+            },label:{
+                Text("Add")
+            })
+        }
     }
     
 }
+
+struct TextFieldCopy: View {
+    
+    var description: String
+    
+    var body: some View {
+        
+        Text(description)
+        
+    }
+    
+}
+/*
+struct EditTaskScreen: View {
+    
+    
+    var body: some View {
+        TextField("Edit this", $description)
+    }
+    
+}
+*/
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
